bc909da2bc151a20347fc778b7c3fec1dad4ad28
Almir10 <karahodzaalmir4@gmail.com>
2025-03-17 16:08:10 +0100

    Initial commit

---
+++ src/test/java/web_portal_zaposljenje/WebPortalZaposljenjeApplicationTests.java
@@ -0,0 +1,13 @@
+package web_portal_zaposljenje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPortalZaposljenjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
