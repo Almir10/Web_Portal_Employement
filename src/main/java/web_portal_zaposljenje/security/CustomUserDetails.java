7641d45ab4db21fd038472cd222a6c699bc9682a
Almir10 <karahodzaalmir4@gmail.com>
2025-04-10 01:22:59 +0200

    Dodate CustomUserDetails i CustomUserDetailsService za implementaciju Spring Security kao i dodate funkcionalnsti u Security Config

---
+++ src/main/java/web_portal_zaposljenje/security/CustomUserDetails.java
@@ -0,0 +1,61 @@
+package web_portal_zaposljenje.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import web_portal_zaposljenje.model.User;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CustomUserDetails implements UserDetails {
+
+    private String email;
+    private String password;
+    private Set<GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user) {
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getRoleName()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
---
+++ src/main/java/web_portal_zaposljenje/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package web_portal_zaposljenje.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import web_portal_zaposljenje.repository.IUserRepository;
+import web_portal_zaposljenje.model.User;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        if (userOptional.isEmpty()) {
+            throw new UsernameNotFoundException("Korisnik sa emailom: " + email + " nije pronađen.");
+        }
+        User user = userOptional.get();
+        return new CustomUserDetails(user);
+    }
+
+}
